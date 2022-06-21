7855a57e26836f679c438d2383c52b4e327715dd
vishnu-krishnan <vishnuoffcl@gmail.com>
2022-06-21 11:51:49 +0530

    removed JDBC example class and configured to routesBuilder class

--- src/main/java/com/study/projects/MyRouteBuilder.java
+++ src/main/java/com/study/projects/MyRouteBuilder.java
@@ -2,53 +2,64 @@ package com.study.projects;
 
 import org.apache.camel.PropertyInject;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.sql.SqlComponent;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-/**
- * A Camel Java DSL Router
- */
 public class MyRouteBuilder extends RouteBuilder {
 
-	@PropertyInject("{{so.api.path}}")
+	@PropertyInject("{{api.path}}")
 	private String apiPath;
 
-	@PropertyInject("{{so.api.host}}")
+	@PropertyInject("{{api.host}}")
 	private String host;
 
-	@PropertyInject("{{so.api.port}}")
+	@PropertyInject("{{api.port}}")
 	private String port;
-	
-	@Override
-    public void configure() {
 
-		
-		  SqlComponent component = getContext().getComponent("sql",SqlComponent.class);
-		  DriverManagerDataSource dataSource= new DriverManagerDataSource();
-		  dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		  dataSource.setUrl("jdbc://mysql:127.0.0.1:3306/study");
-		  dataSource.setUsername("root"); dataSource.setPassword("root");
-		  component.setDataSource(dataSource);
-		 
+	public void configure() {
 
-		
 		restConfiguration().host(host).port(port).component("jetty");
-    	
-    	rest(apiPath)
-    			.get("/{name}").to("direct:hiRoute")
-    			.post("/{name}").to("direct:helloRoute");
 
-    	from("direct:hiRoute")
-				.log("Input message received: body -> ${body}")
-				.unmarshal().json()
-				.bean(new Calculations(),"addItem")
-				.marshal().json()
-				.transform().simple("${body}");
-    	
-		from("direct:helloRoute")
-				.transform()
-				.simple("Hello ${header.name}");
+		rest(apiPath)
+				.post("/submit")
+				.to("direct:insert");
+		rest(apiPath)
+				.get("/{id}")                                                                                           //.get("/{name}")
+				.to("direct:view")
+				.get("/")
+				.to("direct:view");
+		rest(apiPath)
+				.put("/{id}")
+				.to("direct:update");
+		rest(apiPath)
+				.delete("/{id}")
+				.to("direct:delete");
 
+		/*from("direct:insert")
+				.setBody(simple("insert into customer values(100,'Name')"))
+				.to("jdbc:myDataSource")
+				.log("transformed message body ${body}");*/
+
+		from("direct:view")
+				.log("input message received body-> ${body}")
+				.choice()
+					.when(simple("${headers.id} != null "))
+						.setBody(simple("select name from customer where id =${headers.id}"))
+						.to("jdbc:myDataSource")
+						.marshal().json()
+						.to("log:?level=INFO&showBody=true")
+					.otherwise()
+						.setBody(simple("select name from customer"))
+						.to("jdbc:myDataSource")
+						.marshal().json()
+						.to("log:?level=INFO&showBody=true");
+
+		/*from("direct:update")
+				.setBody(constant("Update customer set name = 'manu' where id='100'"))
+				.to("jdbc:myDataSource")
+				.log("transformed message body ${body}");
+
+		from("direct:delete")
+				.setBody(constant("delete from customer where id = ${id}"))
+				.to("jdbc:myDataSource")
+				.log("transformed message body ${body}");*/
 	}
-
 }
